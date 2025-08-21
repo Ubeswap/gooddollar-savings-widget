98e5f1601bb3cae315f6c6fcb44e2aa0a56bcf9f
Kadir Ay <kadiraydev@gmail.com>
2025-08-21 17:10:54 +0300

    build update for test page

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,10 @@ import { resolve } from 'path';
 
 export default defineConfig({
   build: {
-    lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: 'GooddollarSavingsWidget',
-      fileName: 'gooddollar-savings-widget',
-      formats: ['es', 'umd']
-    },
     outDir: 'dist',
     rollupOptions: {
-      external: ['lit', 'viem'],
-      output: {
-        globals: {
-          lit: 'Lit',
-          viem: 'Viem'
-        }
+      input: {
+        main: resolve(__dirname, 'index.html')
       }
     }
   },
